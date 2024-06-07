2c53af3b2a422341f1fc40c952aabf373e79fa89
AJ <amariuslesure@hotmail.com>
2024-06-07 03:28:21 -0500

    segfault fixed (line 159)

--- db.c
+++ db.c
@@ -5,28 +5,6 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-// establishes how much space to be allocated for usernames and emails
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-
-// defines a quick way to grab the size of an attribute of an object (struct)
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
-// The following constants are for determing how row information will be stored in memory. Each row will contain an ID, a username, and an email, all of which will be adjacent in memory.
-#define ID_SIZE (uint32_t)size_of_attribute(Row, id)
-#define USERNAME_SIZE (uint32_t)size_of_attribute(Row, username)
-#define EMAIL_SIZE (uint32_t)size_of_attribute(Row, email)
-#define ID_OFFSET (uint32_t)0
-#define USERNAME_OFFSET (uint32_t)ID_OFFSET+ID_SIZE
-#define EMAIL_OFFSET (uint32_t)USERNAME_OFFSET+USERNAME_SIZE
-#define ROW_SIZE (uint32_t)ID_SIZE+USERNAME_SIZE+EMAIL_SIZE
-
-// now we do more memory shenanigans to create the Table structure
-#define PAGE_SIZE (uint32_t)4096
-#define TABLE_MAX_PAGES 100
-#define ROWS_PER_PAGE (uint32_t)PAGE_SIZE/ROW_SIZE
-#define TABLE_MAX_ROWS (uint32_t)ROWS_PER_PAGE*TABLE_MAX_PAGES
-
 // Create an InputBuffer object to handle tokenization of user input
 typedef struct {
     char* buffer;
@@ -34,38 +12,11 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
-// method for initializing an InputBuffer object
-InputBuffer* new_input_buffer() {
-    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
-
-    return input_buffer;
-}
-
-// print prompt to the output to indicate user input
-void print_prompt() { printf("db > "); }
-
-// Reads and stores user input
-void read_input(InputBuffer* input_buffer) {
-    // Uses getline() to store input into buffer, buffer size stored as well, and we're reading from stdin
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-
-    if (bytes_read <= 0) {
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Ignore trailing newline or whateva
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
-}
-
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
+// define return values for executing commands
+typedef enum {
+    EXECUTE_TABLE_FULL,
+    EXECUTE_SUCCESS
+} ExecuteResult;
 
 // define return values for processing meta commands, to be used by do_meta_command()
 typedef enum {
@@ -80,16 +31,14 @@ typedef enum {
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
-// define return values for executing commands
-typedef enum {
-    EXECUTE_TABLE_FULL,
-    EXECUTE_SUCCESS
-} ExecuteResult;
-
 typedef enum {
     STATEMENT_INSERT,
     STATEMENT_SELECT
 } StatementType;
+
+// establishes how much space to be allocated for usernames and emails
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 
 typedef struct {
     uint32_t id;
@@ -102,10 +51,32 @@ typedef struct {
     Row row_to_insert;
 } Statement;
 
+// defines a quick way to grab the size of an attribute of an object (struct)
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+// The following constants are for determing how row information will be stored in memory. Each row will contain an ID, a username, and an email, all of which will be adjacent in memory.
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// now we do more memory shenanigans to create the Table structure
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
 typedef struct {
     uint32_t num_rows;
     void* pages[TABLE_MAX_PAGES];
 } Table;
+
+void print_row(Row* row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
 // function that converts Row objects to our compact memory setup
 void serialize_row(Row* source, void* destination) {
@@ -113,7 +84,6 @@ void serialize_row(Row* source, void* destination) {
     memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
     memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
-
 // does literally the opposite
 void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
@@ -136,12 +106,38 @@ void* row_slot(Table* table, uint32_t row_num) {
     return page + byte_offset;
 }
 
+// function that initializes a table
+Table* new_table() {
+    Table* table = (Table*)malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+
+    return table;
+}
+
 // frees all the memory used to create the table
 void free_table(Table* table) {
     for (uint32_t i = 0; table->pages[i]; i++) {
         free(table->pages[i]);
     }
     free(table);
+}
+
+// method for initializing an InputBuffer object
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
 }
 
 // parse meta commands
@@ -160,7 +156,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
         int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", statement->row_to_insert.id, statement->row_to_insert.username, statement->row_to_insert.email
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email
         );
         if (args_assigned < 3) {
             return PREPARE_SYNTAX_ERROR;
@@ -190,6 +186,15 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
     return EXECUTE_SUCCESS;
 }
 
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+  }
+  return EXECUTE_SUCCESS;
+}
+
 ExecuteResult execute_statement(Statement* statement, Table* table) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
@@ -200,15 +205,22 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
     }
 }
 
-// function that initializes a table
-Table* new_table() {
-    Table* table = (Table*)malloc(sizeof(Table));
-    table->num_rows = 0;
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-        table->pages[i] = NULL;
+// print prompt to the output to indicate user input
+void print_prompt() { printf("db > "); }
+
+// Reads and stores user input
+void read_input(InputBuffer* input_buffer) {
+    // Uses getline() to store input into buffer, buffer size stored as well, and we're reading from stdin
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
     }
 
-    return table;
+    // Ignore trailing newline or whateva
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
 }
 
 int main(int arc, char* argv[]) {
