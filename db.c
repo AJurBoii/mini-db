57d42f71100d198bfc4d9772eefa6b7430acd69c
AJ <amariuslesure@hotmail.com>
2024-06-27 19:17:21 -0500

    added node header layout constants

--- db.c
+++ db.c
@@ -42,6 +42,26 @@ typedef enum {
     STATEMENT_SELECT
 } StatementType;
 
+// keeps track of node type for our B-tree data structure
+typedef enum {
+    NODE_INTERNAL,
+    NODE_LEAF
+} NodeType;
+
+// node header layout
+const u_int32_t NODE_TYPE_SIZE = sizeof(u_int8_t);
+const u_int32_t NODE_TYPE_OFFSET = 0;
+const u_int32_t IS_ROOT_SIZE = sizeof(u_int8_t);
+const u_int32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const u_int32_t PARENT_POINTER_SIZE = sizeof(u_int32_t);
+const u_int32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const u_int8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+// leaf node header layout
+const u_int32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(u_int32_t);
+const u_int32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const u_int32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
 // establishes how much space to be allocated for usernames and emails
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
