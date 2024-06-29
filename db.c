7e656f06a9a03f153f044d4f1b6275acece86632
AJ <amariuslesure@hotmail.com>
2024-06-28 20:01:37 -0500

    add create_new_root() function

--- db.c
+++ db.c
@@ -381,6 +381,28 @@ u_int32_t get_unused_page_num(Pager* pager) {
     return pager->num_pages;
 }
 
+// helper for the helper for leaf_node_insert() lol. we already allocated the right child node and moved the upper half of its parent into it.
+// now we just gotta allocate the left child node.
+void create_new_root(Table* table, u_int32_t right_child_page_num) {
+    void* root = get_page(table->pager, table->root_page_num);
+    void* right_child = get_page(table->pager, right_child_page_num);
+    u_int32_t left_child_page_num = get_unused_page_num(table->pager);
+    void* left_child = get_page(table->pager, left_child_page_num);
+
+    // old root copied to left child
+    memcpy(left_child, root, PAGE_SIZE);
+    set_node_root(left_child, false);
+
+    // root page is new internal node. it has a single key and two children
+    initialize_internal_node(root);
+    set_node_root(root, true);
+    *internal_node_num_keys(root) = 1;
+    *internal_node_child(root, 0) = left_child_page_num;
+    u_int32_t left_child_max_key = get_node_max_key(left_child);
+    *internal_node_key(root, 0) = left_child_max_key;
+    *internal_node_right_child(root) = right_child_page_num;
+}
+
 // helper function for leaf_node_insert(); if no space is left on the leaf node, it splits it until an upper and lower node
 void leaf_node_split_and_insert(Cursor* cursor, u_int32_t key, Row* value) {
     /**
