8c815b6a428440e2641bc367b44ef32e892cdf36
AJ <amariuslesure@hotmail.com>
2024-06-30 00:56:57 -0500

    refactor cursor_advance()

--- db.c
+++ db.c
@@ -395,7 +395,15 @@ void cursor_advance(Cursor* cursor) {
 
     cursor->cell_num += 1;
     if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
-        cursor->end_of_table = true;
+        /* Go to next leaf node!! */
+        u_int32_t next_page_num = *leaf_node_next_leaf(node);
+        if (next_page_num == 0) {
+            /* we're at the rightmost leaf!! no more siblings left */
+            cursor->end_of_table = true;
+        } else {
+            cursor->page_num = next_page_num;
+            cursor->cell_num = 0;
+        }
     }
 }
 
